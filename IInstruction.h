b11409c23535997f893e20b84f24ce4be192e04f
1105-Sullivan-Bradley <bradleysullivan@nevada.unr.edu>
2022-01-26 11:15:11 -0800

    Added header documentation

--- IInstruction.h
+++ IInstruction.h
@@ -1,3 +1,15 @@
+/**
+ * @file IInstruction.h
+ * @author Bradley Sullivan (bradleysullivan@nevada.unr.edu)
+ * @brief Defines the IInstruction interface. Different Assembly architecture
+ *        classes should inherit from this interface.
+ * @version 0.1
+ * @date 2022-01-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #ifndef IINSTRUCTION_H
 #define IINSTRUCTION_H
 
